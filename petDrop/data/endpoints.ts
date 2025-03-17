f7a4aa69d77c1a9727171be1451a6d937e384890
unknown <bbryan21@iastate.edu>
2025-03-17 15:36:46 -0500

    all endpoints brought over from backend

--- petDrop/data/endpoints.ts
+++ petDrop/data/endpoints.ts
@@ -1,3 +1,5 @@
+// replace 'localhost' with the ip address of your computer to hit these on expo go
+// ACCOUNT
 export const ADD_ACCOUNT = 'http://localhost:8080/addaccount';
 export const UPDATE_ACCOUNT = 'http://localhost:8080/updateaccount';
 export const UPDATE_ACCOUNT_USERNAME = 'http://localhost:8080/updateaccount/username/';
@@ -11,3 +13,30 @@ export const GET_ACCOUNT_BY_USERNAME = 'http://localhost:8080/getaccountbyuserna
 export const GET_ACCOUNT_BY_EMAIL = 'http://localhost:8080/getaccountbyemail/';
 export const GET_ACCOUNT_BY_PHONE = 'http://localhost:8080/getaccountbyphone/';
 export const GET_ACCOUNT_BY_ID = 'http://localhost:8080/getaccountbyid/';
+// PETS
+export const ADD_PET = 'http://localhost:8080/addpet';
+export const UPDATE_PET = 'http://localhost:8080/updatepet';
+export const UPDATE_PET_NAME = 'http://localhost:8080/updatepet/name/';
+export const UPDATE_PET_IMAGE = 'http://localhost:8080/updatepet/image/';
+export const UPDATE_PET_AGE = 'http://localhost:8080/updatepet/age/';
+export const UPDATE_PET_BREED = 'http://localhost:8080/updatepet/breed/';
+export const UPDATE_PET_ADDRESS = 'http://localhost:8080/updatepet/address/';
+export const GET_ALL_PETS = 'http://localhost:8080/getallpets';
+export const GET_PET_BY_ID = 'http://localhost:8080/getpetbyid/';
+// MEDICATIONS
+export const ADD_MEDICATION = 'http://localhost:8080/addmedication';
+export const UPDATE_MEDICATION = 'http://localhost:8080/updatemedication';
+export const UPDATE_MEDICATION_NAME = 'http://localhost:8080/updatemedication/name/';
+export const UPDATE_MEDICATION_COLOR = 'http://localhost:8080/updatemedication/color/';
+export const UPDATE_MEDICATION_DESCRIPTION = 'http://localhost:8080/updatemedication/description/';
+export const UPDATE_MEDICATION_DATES = 'http://localhost:8080/updatemedication/dates/';
+export const UPDATE_MEDICATION_RANGE = 'http://localhost:8080/updatemedication/range/';
+export const GET_ALL_MEDICATIONS = 'http://localhost:8080/getallmedications';
+export const GET_MEDICATION_BY_ID = 'http://localhost:8080/getmedicationbyid/';
+// REMINDERS
+export const ADD_REMINDER = 'http://localhost:8080/addreminder';
+export const UPDATE_REMINDER = 'http://localhost:8080/updatereminder';
+export const UPDATE_REMINDER_MEDICATION = 'http://localhost:8080/updatereminder/medication/';
+export const UPDATE_REMINDER_PET = 'http://localhost:8080/updatereminder/pet/';
+export const GET_ALL_REMINDERS = 'http://localhost:8080/getallreminders';
+export const GET_REMINDER_BY_ID = 'http://localhost:8080/getreminderbyid/';
