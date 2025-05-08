7c01ccf4b2c76efcdadda798f23d2f033ef16c8d
blakebryan <bbryan0316@gmail.com>
2025-05-08 18:30:14 -0500

    maybe added underlines on the calendar for meds dates - needs to be tested

--- petDrop/screens/Home.tsx
+++ petDrop/screens/Home.tsx
@@ -9,7 +9,7 @@ import { Color, ScreenEnum } from "../GlobalStyles";
 import { styles, calendarTheme } from "../styles/Home.styles";
 import ReminderPopup from "../components/ReminderPopup";
 import { Account, emptyMed, emptyPet, emptyReminder, Reminder } from "../data/dataTypes";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const { width, height } = Dimensions.get("window");
 
@@ -23,6 +23,36 @@ const Home = ({ navigation, route }: HomeProps) => {
 
   // store the user's account info to avoid typing "route.params.account" repeatedly
   let account: Account = route.params.account;
+
+  let markedDatesMap: Map<string, Array<{ color: string, startingDay?: boolean, endingDay?: boolean }>> = new Map();
+  let markedDates: any = {};
+  useEffect(() => {
+    account.pets.forEach(pet => {
+      pet.medications.forEach(med => {
+        med.dates.forEach(date => {
+          const periods = markedDatesMap.get(date);
+          let newPeriod: { color: string, startingDay?: boolean, endingDay?: boolean } = { color: med.color };
+          let dayBefore: string, dayAfter: string;
+          let dateObject: Date = new Date(date);
+          dateObject.setTime(dateObject.getTime() - 86400000);
+          dayBefore = dateObject.toISOString();
+          dateObject.setTime(dateObject.getTime() + (86400000 * 2));
+          dayAfter = dateObject.toISOString();
+          if (!med.dates.includes(dayBefore)) {
+            newPeriod.startingDay = true;
+          }
+          if (!med.dates.includes(dayAfter)) {
+            newPeriod.endingDay = true;
+          }
+          periods ? markedDatesMap.set(date, periods.concat([newPeriod])) : markedDatesMap.set(date, [newPeriod]);
+        })
+      });
+    });
+
+    markedDatesMap.forEach((periods, date) => {
+      markedDates[`${date}`] = { periods: periods }
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -39,9 +69,8 @@ const Home = ({ navigation, route }: HomeProps) => {
             markingType='multi-period'
             style={styles.calendar}
             theme={calendarTheme}
-            onDayPress={(day: DateData) => { console.log(day)}}
-            markedDates={{
-            }}
+            onDayPress={(day: DateData) => { console.log(day) }}
+            markedDates={markedDates}
           />
         </View>
 
